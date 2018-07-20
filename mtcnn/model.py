b30f41e0603d18fe6448901a819724dd5f365adb
dew.maple <dew.maple@gmail.com>
2018-07-20 13:39:15 +0800

    first commit: build model

---
+++ mtcnn/model.py
@@ -0,0 +1,68 @@
+from keras import Input, layers, Model
+
+
+def p_net():
+    x = Input(shape=(12, 12, 3))
+    y = layers.Conv2D(10, 3, padding='valid', strides=(1, 1), activation='relu', name='p_conv1')(x)
+    y = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='p_max_pooling1')(y)
+    y = layers.Conv2D(16, 3, padding='valid', strides=(1, 1), activation='relu', name='p_conv2')(y)
+    y = layers.Conv2D(32, 3, padding='valid', strides=(1, 1), activation='relu', name='p_conv3')(y)
+
+    classifier = layers.Conv2D(2, 1, activation='softmax', name='p_classifier')(y)
+    bbox = layers.Conv2D(4, 1, name='p_bbox')(y)
+    landmark = layers.Conv2D(10, 1, padding='valid', name='p_landmark')(y)
+
+    model = Model(inputs=[x], outputs=[classifier, bbox, landmark], name='P_Net')
+
+    return model
+
+
+def r_net():
+    x = Input(shape=(24, 24, 3))
+    y = layers.Conv2D(28, 3, padding='same', strides=(1, 1), activation='relu', name='r_conv1')(x)
+    y = layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='p_max_pooling1')(y)
+    y = layers.Conv2D(48, 3, padding='valid', strides=(1, 1), activation='relu', name='r_conv2')(y)
+    y = layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='p_max_pooling2')(y)
+    y = layers.Conv2D(64, 2, padding='valid', activation='relu', name='r_conv3')(y)
+    y = layers.Dense(128, activation='relu', name='r_dense')(y)
+    y = layers.Flatten()(y)
+
+    classifier = layers.Dense(2, activation='softmax', name='r_classifier')(y)
+    bbox = layers.Dense(4, name='r_bbox')(y)
+    landmark = layers.Dense(10, name='r_landmark')(y)
+
+    model = Model(inputs=[x], outputs=[classifier, bbox, landmark], name='R_Net')
+
+    return model
+
+
+def o_net():
+    x = Input(shape=(48, 48, 3))
+    y = layers.Conv2D(32, 3, padding='same', strides=(1, 1), activation='relu', name='o_conv1')(x)
+    y = layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='o_max_pooling1')(y)
+    y = layers.Conv2D(64, 3, padding='valid', strides=(1, 1), activation='relu', name='o_conv2')(y)
+    y = layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='o_max_pooling2')(y)
+    y = layers.Conv2D(64, 3, padding='valid', strides=(1, 1), activation='relu', name='o_conv3')(y)
+    y = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='o_max_pooling3')(y)
+    y = layers.Conv2D(128, 2, padding='valid', strides=(1, 1), activation='relu', name='o_conv4')(y)
+    y = layers.Dense(256, activation='relu', name='o_dense')(y)
+    y = layers.Flatten()(y)
+
+    classifier = layers.Dense(2, activation='softmax', name='o_classifier')(y)
+    bbox = layers.Dense(4, name='o_bbox')(y)
+    landmark = layers.Dense(10, name='o_landmark')(y)
+
+    model = Model(inputs=[x], outputs=[classifier, bbox, landmark], name='O_Net')
+
+    return model
+
+
+if __name__ == '__main__':
+    p = p_net()
+    p.summary()
+
+    r = r_net()
+    r.summary()
+
+    o = o_net()
+    o.summary()
